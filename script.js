2c2ecface883ef26ccec68d6b7811ddc3fec3713
Aleksandar Pletl <pletl.aleksandar@gmail.com>
2024-07-14 19:54:46 +0200

    Scroll on click

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 window.addEventListener(
-  "scroll",
-  () => {
-    document.body.style.setProperty(
-      "--scroll",
-      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
-    );
-  },
-  false
+    "scroll",
+    () => {
+        document.body.style.setProperty(
+            "--scroll",
+            window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+        );
+    },
+    false
 );
+
+var pageHeight = window.innerHeight;
+
+function scrollToPersonal() {
+    window.scrollTo(0, 0);
+}
+
+function scrollToEducation() {
+    let windowHeight = window.innerHeight;
+    // window.scrollBy(0, 0);
+    window.scrollTo(0, windowHeight);
+}
+
+function scrollToSkills() {
+    let windowHeight = window.innerHeight;
+    // window.scrollBy(0, 0);
+    window.scrollTo(0, 2 * windowHeight);
+}
+
+function scrollToExperience() {
+    let windowHeight = window.innerHeight;
+    // window.scrollBy(0, 0);
+    window.scrollTo(0, 3 * windowHeight);
+}
+
+function scrollToAbout() {
+    let windowHeight = window.innerHeight;
+    window.scrollTo(0, 4 * windowHeight);
+}
